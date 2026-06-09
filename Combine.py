964c1d71be0aede354cafa929ce572b23a6acb5a
kenricli <kenricli.work@gmail.com>
2026-06-08 22:33:13 -0700

    Update Combine.py

--- Combine.py
+++ Combine.py
@@ -194,7 +194,7 @@ elif st.session_state.active_calculator == "FUDR":
 
         st.subheader("✏️ To Fill Out Admin Instructions")
         admin_text = (
-            f"1. **Floxuridine dose:** {dose_rate:g} mg/kg/day × {dosing_weight:.1f} kg = **Daily dose of Floxuridine:** {daily_dose:.2f} mg/day  \n"
+            f"1. **Floxuridine dose:** {dose_rate:g} mg/kg/day × {dosing_weight:.g} kg = **Daily dose of Floxuridine:** {daily_dose:.2f} mg/day  \n"
             f"2. **Daily dose of Floxuridine:** {daily_dose:.2f} mg/day / **flow rate:** {flow_rate} mL/day = **pump concentration:** {pump_concentration:.2f} mg/mL  \n"
             f"3. **Pump concentration:** {pump_concentration:.2f} mg/mL × **pump volume:** {int(pump_volume)} mL = **total dose of FLOXURIDINE:** {final_fudr_dose} mg (rounded to closest 5 mg)  \n"
             f"4. Please insert total dose into Floxuridine dosing field above"
