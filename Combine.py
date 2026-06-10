5c89bf52688abea42bbe452c672ecf46e988e9bd
kenricli <kenricli.work@gmail.com>
2026-06-09 23:43:22 -0700

    Update Combine.py

--- Combine.py
+++ Combine.py
@@ -214,8 +214,7 @@ def render_fudr_calculator():
         dose_rate = st.number_input("Enter Custom Dose (mg/kg)", min_value=0.00, max_value=0.12, value=None) if dose_selection == "Custom..." else dose_selection
     with col2:
         real_weight = st.number_input("Patient Weight (kg)", min_value=0.0, max_value=250.0, value=None, format="%g", placeholder="Enter weight...")
-        flow_selection = st.selectbox("Pump Flow Rate (mL/day)", options=[1.4, 1.3, 1.2, 1.1, "Custom..."], index=1, format_func=lambda x: f"{x} mL/day" if isinstance(x, (int, float)) else x)
-        flow_rate = st.number_input("Enter Custom Flow Rate (mL/day)", min_value=0.00, max_value=2.00, value=None) if dose_selection == "Custom..." else flow_selection
+        flow_rate = st.selectbox("Pump Flow Rate (mL/day)", options=[1.4, 1.3, 1.2, 1.1], index=1, format_func=lambda x: f"{x} mL/day")
     with col3:
         height_cm = st.number_input("Patient Height (cm)", min_value=0.0, max_value=250.0, value=None, format="%g", placeholder="Enter height...")
         st.metric(label="Pump Volume (Fixed)", value=f"{int(pump_volume)} mL")
